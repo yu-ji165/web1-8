cce811bb3ee2c781b5f6e9431ef9b0c076fb8b48
yu-ji <enpatsushakugan@icloud.com>
2018-07-02 17:42:07 +0900

    ユニティずるすぎ（いまさら）

---
+++ Igaguri/Assets/igaguriG.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class igaguriG : MonoBehaviour
+{
+    public GameObject igaguriP;
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            GameObject igaguri = Instantiate(igaguriP) as GameObject;
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            Vector3 worldDir = ray.direction;
+            igaguri.GetComponent<igaguriC>().Shoot(worldDir.normalized * 2000);
+        }
+    }
+}
